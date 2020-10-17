796024eb754d1d2decd207aeef5e8375d90cb034
Joe <joe@yis-macbook-pro.lan>
2020-10-16 17:02:12 -0700

    Test environment to bring in python 3.7

--- dockerfile
+++ dockerfile
@@ -8,3 +8,4 @@ RUN apt-get update
 
 RUN apt-get install -y sudo
 
+RUN sudo apt-get install -y python3.7
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #docker run -i -d --privileged -t --name sparkify-container sparkify /bin/bash
 docker run --name sparkify-container -e POSTGRES_PASSWORD=password -d sparkify
-docker exec -it sparkify-container sudo -i -u postgres
+docker exec -it sparkify-container sudo -i
+docker exec -it spakrify-container /usr/bin/python3.7
